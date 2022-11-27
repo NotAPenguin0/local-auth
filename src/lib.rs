84360b804f18fe00c9ed78f66e991acdad8fd3d4
= <=>
2022-11-27 16:58:15 +0100

    print received values

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,13 @@ use hyper;
 use std::convert::Infallible;
 use std::sync::Arc;
 use hyper::{Request, Body, Response};
-use futures::channel::oneshot;
-use futures::channel::oneshot::Sender;
+use futures::channel::mpsc::Sender;
+use futures::{Stream, StreamExt};
 use crate::error::Error;
 
 /// Local HTTP server that listens for authentication requests on a local port.
 pub struct AuthListener {
     address: std::net::SocketAddr
-}
-
-struct HandlerContext {
-    sender: Arc<Option<Sender<String>>>
 }
 
 impl AuthListener {
@@ -32,8 +28,7 @@ impl AuthListener {
     /// Listen for messages, and return the first oauth authentication code received.
     pub async fn listen(&mut self) -> anyhow::Result<String> {
         // Sender/Receiver channel for our authentication code.
-        let (sender, receiver) = oneshot::channel::<String>();
-        let sender = Arc::new(Some(sender));
+        let (sender, mut receiver) = futures::channel::mpsc::channel(1);
         let make_service = hyper::service::make_service_fn(move |_conn| {
             let sender = sender.clone();
             let service = hyper::service::service_fn(move |req| {
@@ -45,22 +40,20 @@ impl AuthListener {
         let server = hyper::Server::try_bind(&self.address)?.serve(make_service);
         let mut auth_code: Option<String> = None;
         let server = server.with_graceful_shutdown(async {
-            let value = receiver.await.unwrap();
-            auth_code = Some(value);
+            let value = receiver.next().await;
+            if let Some(v) = &value {
+                println!("key: {:?}", v);
+            }
+            auth_code = value;
         });
 
         server.await?;
         auth_code.ok_or(anyhow::Error::new(Error::NoAuthCode))
     }
 
-    async fn request_handler(sender: Arc<Option<Sender<String>>>, req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    async fn request_handler(mut sender: Sender<String>, req: Request<Body>) -> Result<Response<Body>, Infallible> {
         println!("Received request: {:?}", hyper::body::to_bytes(req.into_body()).await.unwrap());
-        let mut my_sender: Option<Sender<String>> = None;
-        std::mem::swap(&mut sender.as_ref(), &mut &*&mut my_sender);
-        if let Some(sender) = my_sender {
-            sender.send("THE KEY".to_string()).unwrap();
-        }
-
+        sender.try_send("THE KEY".to_string()).unwrap();
         Ok(Response::new("Authentication successful. You can close this browser window.".into()))
     }
 }
